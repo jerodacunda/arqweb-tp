006498732e7ee2bc848da1e96bf849f419924f52
jerodacunda <jero.d.r@hotmail.com>
2024-11-08 03:41:31 -0300

    mapa carga bien

--- ver-la-carta/src/app/app.component.ts
+++ ver-la-carta/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ver-la-carta';
--- ver-la-carta/src/app/map/map.component.ts
+++ ver-la-carta/src/app/map/map.component.ts
@@ -3,17 +3,23 @@ import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
+  standalone: true,
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
-  private map;
+  private map: any;
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+    this.map = L.map('map').setView([ -34.750847, -58.387544 ], 10);
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
+
+    tiles.addTo(this.map);
   }
 
   constructor() { }
