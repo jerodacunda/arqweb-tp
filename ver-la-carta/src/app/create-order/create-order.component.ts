d607e5b914665a08177297c71a8cd647583d8cc3
jerodacunda <jero.d.r@hotmail.com>
2024-11-14 19:04:27 -0300

    Componente de pedidos muestra detalles y menu, desde mapa o desde QR

--- ver-la-carta/src/app/create-order/create-order.component.ts
+++ ver-la-carta/src/app/create-order/create-order.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MarkerService } from '../marker.service';
 
 @Component({
   selector: 'app-create-order',
@@ -11,33 +12,58 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./create-order.component.scss']
 })
 
-export class CreateOrderComponent {
+export class CreateOrderComponent implements OnInit {
   @Input() localId: number | null = null;
   @Input() tableNumber: number | null = null;
   orderDetails: string = '';
+  localDetails: any = null;
+  menuPdfUrl: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private markerService: MarkerService) {}
+
+  ngOnInit(): void {
+    if (this.localId !== null) {
+      this.loadLocalDetails(this.localId);
+    }
+  }
+
+  private loadLocalDetails(localId: number): void {
+    this.markerService.getLocales().subscribe(
+      (locales: any[]) => {
+        // Filtrar el local que coincide con el `localId`
+        const local = locales.find(l => l.id === localId);
+
+        if (local) {
+          this.localDetails = local;
+          this.menuPdfUrl = `http://localhost:8000/${local.menu_pdf}`;
+        } else {
+          console.error('No se encontró un local con el ID especificado');
+        }
+      },
+      error => {
+        console.error('Error al cargar los detalles del local:', error);
+      }
+    );
+  }
 
   onSubmit(): void {
     if (this.localId && this.tableNumber) {
       const orderData = {
         table_number: this.tableNumber,
-        order: {description: this.orderDetails, status: 'Pending'},
+        order: { description: this.orderDetails, status: 'Pending' },
       };
 
       this.http.post(`http://localhost:8000/api/locales/${this.localId}/tables-orders/`, orderData)
         .subscribe(
           response => {
             console.log('Pedido creado con éxito:', response);
-            alert('Pedido creado con éxito');
+            alert('Pedido realizado con éxito');
           },
           error => {
-            console.error('Error al crear el pedido:', error);
-            alert('Hubo un error al crear el pedido');
+            console.error('Error al realizar el pedido:', error);
+            alert('Ocurrió un error al realizar el pedido');
           }
         );
-    } else {
-      alert('Debe seleccionar un número de mesa y detalles del pedido');
     }
   }
 }
