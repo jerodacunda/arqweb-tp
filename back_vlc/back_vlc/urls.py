e2ca62b09b3b0dc0aa4a3d97a9677d705a400d3b
jerodacunda <jero.d.r@hotmail.com>
2024-11-20 15:36:12 -0300

    agrego urlpattern de entrada

--- back_vlc/back_vlc/urls.py
+++ back_vlc/back_vlc/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/')),  # Redirigir a /api/
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
