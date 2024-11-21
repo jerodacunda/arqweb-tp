1f29cf0083289f5695e7eea11b495e5f751e6045
Belen <bloleo@dc.uba.ar>
2024-11-21 03:12:26 -0300

    qr_generator fixed

--- back_vlc/qr_generator.py
+++ back_vlc/qr_generator.py
@@ -1,8 +1,8 @@
 import qrcode
 
-local_id = 16
+local_id = 1
 num_tables = 2  # Número de mesas
 for i in range(1, num_tables + 1):
-    qr_data = f"http://localhost:4200/?mode=user&localId={local_id}&tableNumber={i}"
+    qr_data = f"https://arqweb-tp-angular.onrender.com/?mode=user&localId={local_id}&tableNumber={i}"
     qr = qrcode.make(qr_data)
-    qr.save(f"back_vlc/qr_for_tables/local_{local_id}_table_{i}.png")
+    qr.save(f"qr_for_tables/local_{local_id}_table_{i}.png")
